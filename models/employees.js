f4063441f26beef09663f841060c2923ebe4f8f0
Peet <6811850001@mut.ac.th>
2026-03-11 12:59:31 +0700

    first commit

---
+++ models/employees.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+const employeeSchema = new mongoose.Schema({
+    
+    name: {
+        type: String,
+        required: true
+    },
+
+    gender: {
+        type: String,
+        enum: ["ชาย", "หญิง"]
+    },
+
+    position: {
+        type: String
+    },
+
+    phone: {
+        type: String
+    },
+
+    email: {
+        type: String, required: true, unique: true 
+    },
+
+    salary: {
+        type: Number
+    },
+
+    startdate: {
+        type: Date
+    },
+
+    role: {
+        type: String,
+        enum: ["employee", "manager"],
+        default: "employee" // employee หรือ manager
+    },
+
+    status: {
+        type: String,
+        default: "active"
+    }
+})
+
+module.exports = mongoose.model('Employee', employeeSchema)
---
+++ models/orders.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const OrderSchema = new mongoose.Schema({
+
+    product_id: String,
+    product_name: String,
+
+    price: Number,
+    qty: Number,
+    total: Number,
+
+    size: String,
+
+    payment_method: String,
+
+    order_date:{
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+module.exports = mongoose.model('Order', OrderSchema)
